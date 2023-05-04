8046a3109a39383169d05d087fb88024f3d60366
Adinubaeze <emmanuelezeadinuba@gmail.com>
2023-05-04 10:33:14 +0100

    Gsouund music platform

---
+++ src/Components/LeftMenu.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "../styles/LeftMenu.css";
+import { FaSpotify, FaEllipsisH } from "react-icons/fa";
+import { BiSearchAlt } from "react-icons/bi";
+import { Menu } from "./Menu";
+import { MenuList } from "./MenuList";
+import { MenuPlayList } from "./MenuPlayList";
+import TrackList from "./TrackList";
+import {RiNeteaseCloudMusicFill} from 'react-icons/ri'
+function LeftMenu() {
+  return (
+    <div className="leftMenu">
+      <div className="logoContainer">
+        <div className="logo">
+          <i>
+            <RiNeteaseCloudMusicFill/>
+          </i>
+
+          <h2>GSound</h2>
+        </div>
+
+        <i>
+          <FaEllipsisH />
+        </i>
+      </div>
+
+      <div className="searchBox">
+        <input type="text" placeholder="Search..." />
+        <i>
+          <BiSearchAlt />
+        </i>
+      </div>
+
+      <Menu title={"Menu"} listObject={MenuList} />
+
+      <MenuPlayList />
+
+      <TrackList trackName={"Take On Me"} artistName={"A-ha"} />
+    </div>
+  );
+}
+
+export { LeftMenu };
